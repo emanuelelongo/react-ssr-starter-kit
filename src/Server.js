5efb51af3668f61df4a11a3cdd6a33bb534a41f3
Emanuele Longo <emanuele.longo@ynap.com>
2018-05-15 17:07:39 +0200

    passing query string to requirements

--- src/Server.js
+++ src/Server.js
@@ -43,8 +43,8 @@ export default class Server {
 
     this.server.use((req, res) => {
       const store = createStore(this.config.rootReducer, applyMiddleware(thunk));
-      const components = matchRouteComponents(req.path, createRouterConfig(this.config.routes));	
-      const componentDataPromise = fetchComponentData(store.dispatch, components, req.path);
+      const components = matchRouteComponents(req.path, createRouterConfig(this.config.routes));
+      const componentDataPromise = fetchComponentData(store.dispatch, components, req.path, req.query);
       const headers = pick(req.headers, this.config.headersToForward);
       const layoutPromise = this.layoutEngine.resolveLayout(req, {headers});
 
--- src/helpers/fetchComponentData.js
+++ src/helpers/fetchComponentData.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import matchPath from 'react-router/matchPath';
 
-function dispatchIfMatch(dispatch, requirement, path) {
+function dispatchIfMatch(dispatch, requirement, path, query) {
   const match = matchPath(path, requirement[0]);
   return match
-    ? dispatch(requirement[1](match.params))
+    ? dispatch(requirement[1](match.params, query))
     : Promise.resolve();
 }
 
-export default function fetchComponentData(dispatch, components, path) {
+export default function fetchComponentData(dispatch, components, path, query) {
   const requirements = components.reduce( (prev, current) => {
     return current ? (current.requirements || []).concat(prev) : prev;
   }, []);
   const promises = requirements.map(requirement => {
     return typeof(requirement) === 'function'
       ? dispatch(requirement())
-      : dispatchIfMatch(dispatch, requirement, path);
+      : dispatchIfMatch(dispatch, requirement, path, query);
   });
   return Promise.all(promises);
 }
